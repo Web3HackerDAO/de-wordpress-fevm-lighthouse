fe9b36ef325c50441d11965006b15978fc5df7d9
Web3HackerDAO <web3hackerdao@gmail.com>
2023-02-06 06:24:09 +0800

    ✨ add pricing blog about page

---
+++ docs/.vitepress/theme/components/Team.vue
@@ -0,0 +1,29 @@
+<script setup lnag="ts">
+import {
+  VPTeamPage,
+  VPTeamPageTitle,
+  VPTeamMembers
+} from 'vitepress/theme'
+
+</script>
+<template>
+  <VPTeamPage>
+    <VPTeamPageTitle>
+      <template #title>
+        {{ $frontmatter.team.title }}
+      </template>
+      <template #lead>
+        {{ $frontmatter.team.description }}
+      </template>
+    </VPTeamPageTitle>
+    <div class="flex flex-col max-w-4xl gap-3 px-6 mx-auto mb-16 lg:px-0">
+      <h2 class="text-3xl font-bold ">
+        {{ $frontmatter.team.subTitle }}
+      </h2>
+      <p class="text-lg leading-relaxed">
+        {{ $frontmatter.team.subDescription }}
+      </p>
+    </div>
+    <VPTeamMembers :members="$frontmatter.team.members" />
+  </VPTeamPage>
+</template>
